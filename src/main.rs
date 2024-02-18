72bedc71b7837b4bf5ad3a1922b4c60073e3899e
icudev <icudev1@proton.me>
2024-02-18 03:15:51 +0100

    Add functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use dotenv::dotenv;
+use serde_json;
+use std::collections::HashMap;
+use wallpaper;
+
+fn get_image(api_key: &String) -> Option<String> {
+    let client = reqwest::blocking::Client::new();
+
+    if let Ok(res) = client
+        .get("https://api.thecatapi.com/v1/images/search")
+        .header("x-api-key", api_key)
+        .send()
+        {
+            let response: String = res.text().unwrap();
+
+            let json: Vec<HashMap<String, serde_json::Value>> = serde_json::from_str(&response).unwrap();
+            let image_url: String = json[0]["url"].to_string().replace("\"", "");
+
+            return Some(image_url);
+        }
+    
+    return None;
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let api_key: String = std::env::var("THECATAPI_KEY").expect("Couldn\'t find api key in .env");
+
+    loop {
+        if let Some(image_url) = get_image(&api_key) {
+            wallpaper::set_from_url(&image_url).unwrap();
+        }
+
+        std::thread::sleep(std::time::Duration::from_secs(3600));
+    }
 }
